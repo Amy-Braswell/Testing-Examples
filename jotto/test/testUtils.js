3207e0196c8fde3a54724a64c61b782834bd3c5e
AmyBraswell <braswellamy@mac.com>
2020-09-07 12:35:42 -0500

    set up guessWords tests

--- jotto/test/testUtils.js
+++ jotto/test/testUtils.js
@@ -1,7 +1,8 @@
 import checkPropTypes from 'check-prop-types'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 
 import rootReducer from '../src/reducers'
+import { middlewares } from '../src/configureStore'
 
 
 /** Factory function to create a ShallowWrapper for the Congrats component
@@ -10,7 +11,8 @@ import rootReducer from '../src/reducers'
  * @returns {Store} - Redux store
  */
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState)
+  const createStoreWithMiddleware= applyMiddleware(...middlewares)(createStore)
+  return createStoreWithMiddleware(rootReducer, initialState)
 }
 
 /**
