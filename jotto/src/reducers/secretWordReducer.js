c777a51276f55d322f98d30b18af91cf7a96bd6f
AmyBraswell <braswellamy@mac.com>
2020-09-07 13:46:35 -0500

    added Thunk for  piece of state

---
+++ jotto/src/reducers/secretWordReducer.js
@@ -0,0 +1,6 @@
+export default (state = null, action) => {
+    // this reducer will get called many times
+    // we need to make sure it doesn't null out an existing state
+    // so make sure to return state
+    return state
+}
