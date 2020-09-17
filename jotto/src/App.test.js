d547f8542c68b79df8dbc342628491fbb7f06051
AmyBraswell <braswellamy@mac.com>
2020-09-17 07:03:34 -0500

    Connect App component and tests

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -11,33 +11,34 @@ import App, { UnconnectedApp } from './App';
  */
 const setup = (state={}) => {
   const store = storeFactory(state);
+  // dive to get actual store from connected App component
   const wrapper = shallow(<App store={store} />).dive().dive();
   return wrapper;
 }
 
 describe('redux properties', () => {
   test('has access to `success` state', () => {
-    // const success = true;
-    // const wrapper = setup({ success });
-    // const successProp = wrapper.instance().props.success;
-    // expect(successProp).toBe(success);
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
   });
   test('has access to `secretWord` state', () => {
-    // const secretWord = 'party';
-    // const wrapper = setup({ secretWord });
-    // const secretWordProp = wrapper.instance().props.secretWord;
-    // expect(secretWordProp).toBe(secretWord);
+    const secretWord = 'party';
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
   });
   test('has access to `guessedWords` state', () => {
-    // const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
-    // const wrapper = setup({ guessedWords });
-    // const guessedWordsProp = wrapper.instance().props.guessedWords;
-    // expect(guessedWordsProp).toEqual(guessedWords);
+    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toEqual(guessedWords);
   });
   test('`getSecretWord` action creator is a function on the props', () => {
-    // const wrapper = setup();
-    // const getSecretWordProp = wrapper.instance().props.getSecretWord;
-    // expect(getSecretWordProp).toBeInstanceOf(Function);
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
   });
 });
 
